9e5c975fc7bbab30ee0c819001f91e0eef96222c
Euan Smith <euan1971@yahoo.co.uk>
2018-04-10 10:18:02 +0100

    improved the inline documentation

--- prop.js
+++ prop.js
@@ -3,6 +3,20 @@
  */
 
 /**
+ * @name PropD
+ * @function
+ * @param {*|function} Value A value to be assigned or a factory function to generate a value 
+ * @param {boolean} isMethod If Value is a function then it is taken as a factory function unless isMethod is true.
+ * @returns {PropD}
+ * @property {PropD} hidden Returns a descriptor with ennumerable = false
+ * @property {PropD} visible Returns a descriptor with ennumerable = true
+ * @property {PropD} constant Returns a descriptor with writable = false
+ * @property {PropD} variable Returns a descriptor with writable = true
+ * @property {PropD} frozen Returns a descriptor with configurable = false
+ * @property {PropD} thawed Returns a descriptor with configurable = true
+ */
+
+/**
  *
  * The prototype for the prop object
  * all methods are chainable
@@ -57,6 +71,7 @@ const defaults = {
 };
 
 // Reduce the prototype to a property descriptor object
+// This enables the methods to be added to an object with a single call to defineProperties
 const propDesc = Object.keys(propProto).reduce(function(d,k){
   //for each prototype property get the descriptor and add it to the descriptor object
   d[k]=Object.getOwnPropertyDescriptor(propProto,k);
@@ -65,27 +80,57 @@ const propDesc = Object.keys(propProto).reduce(function(d,k){
   return d;
 },{});
 
-// Make a clone of a prop object
+/**
+ * The next two methods enable the core functionality by creating new instances of the prop object
+ * As new prop instances are created through chaining an existing instance, this inevitably means
+ * that they call each other.
+ */
+
+
+
+/**
+ * Make a clone of a prop object
+ * @function
+ * @param {PropD} source A prop object to clone
+ * @returns {PropD}
+ */
 function clone(source) {
+  //Get a new base PropD function instance
   const target = makeTarget();
+  //Compose the PropD methods onto the function using the property descriptor generated above
   Object.defineProperties(target, propDesc);
+  //Copy the properties from the source prop object to the target
   target._enum(source.enumerable);
   target._writ(source.writable);
   target._conf(source.configurable);
+  //The value property needs to be copied as a descriptor (as it might be a factory function)
   Object.defineProperty(target, 'value',
     Object.getOwnPropertyDescriptor(source, 'value')
   );
+  //done. Return the new PropD instance.
   return target;
 }
 
-// Make a new base prop object
+/**
+ * Make a new base prop function
+ * @function
+ * @returns {PropD}
+ */
 function makeTarget() {
+  //Return a new prop function instance
+  //This function provides the core PropD functionality
+  //Calling it creates a new PropD instance with the value set
   return function prop (val, isMethod) {
+    //get a new PropD instance to return which is a clone of this one
     var newProp = clone(prop);
+	//Check if the val is a factory function
     const isFactory = typeof val === 'function' && !isMethod;
+	//define the value property - a getter if it is a factory function, a plane value if not
     Object.defineProperty(newProp, 'value', isFactory ? {get: val} : {value: val});
+	//done. Return the new PropD instance.
     return newProp;
   };
 }
 
+//Export the default PropD instance, based on the defaults object.
 module.exports = clone(defaults);
